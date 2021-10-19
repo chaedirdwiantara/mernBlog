1ef545ae4842f6819b01a3ab233e5f01159e0571
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-10-19 15:52:39 +0700

    styling mainApp, header, footer, add some svg icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,22 @@
 import RegisterBg from "./image/register-bg.jpg";
 import LoginBg from "./image/login-bg.jpg";
+import logoChaedir from "./image/logo-chaedir-coding.png";
+// icon
+import ICFacebook from "./icon/facebook.svg";
+import ICTwitter from "./icon/twitter.svg";
+import ICInstagram from "./icon/instagram.svg";
+import ICTelegram from "./icon/telegram.svg";
+import ICDiscord from "./icon/discord.svg";
+import ICGithub from "./icon/github.svg";
 
-export { RegisterBg, LoginBg };
+export {
+  RegisterBg,
+  LoginBg,
+  logoChaedir,
+  ICFacebook,
+  ICTwitter,
+  ICInstagram,
+  ICTelegram,
+  ICDiscord,
+  ICGithub,
+};
