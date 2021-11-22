fcda0d451a7bc515cc46e85a1e6ad6c6acfe1e9f
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-11-22 07:37:12 +0700

    change data home static into dynamic(data response API)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
+  const [dataBlog, setDataBlog] = useState([]);
   useEffect(() => {
     axios
-      .get("http://localhost:4000/v1/blog/posts")
+      .get("http://localhost:4000/v1/blog/posts?page=4&perPage=2")
       .then((result) => {
-        console.log("data API", result.data);
+        console.log(result.data, "response api");
+        const responseAPI = result.data;
+
+        setDataBlog(responseAPI.data);
       })
       .catch((err) => {
         console.log("error: ", err);
       });
-  });
+  }, []);
+
   const history = useHistory();
   return (
     <div className="home-page-wrapper">
@@ -26,10 +31,18 @@ const Home = () => {
       </div>
       <Gap height={20} />
       <div className="content-wrapper">
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        {dataBlog.map((blog) => {
+          return (
+            <BlogItem
+              key={blog._id}
+              image={`http://localhost:4000/${blog.image}`}
+              title={blog.title}
+              body={blog.body}
+              name={blog.author.name}
+              date={blog.createdAt}
+            />
+          );
+        })}
       </div>
       <div className="pagination">
         <Button title="Previous" />
