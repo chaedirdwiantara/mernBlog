43ccd0c90a2b8649bb3b4c60c7d082cde4ac7344
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-11-26 09:33:30 +0700

    implement pagination on home Page

--- src/config/redux/action/homeAction.js
+++ src/config/redux/action/homeAction.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 
-export const setDataBlog = () => (dispatch) => {
+export const setDataBlog = (page) => (dispatch) => {
+  console.log(page, "page");
   axios
-    .get("http://localhost:4000/v1/blog/posts?page=4&perPage=2")
+    .get(`http://localhost:4000/v1/blog/posts?page=${page}&perPage=2`)
     .then((result) => {
       const responseAPI = result.data;
-
+      console.log(responseAPI);
       dispatch({ type: "UPDATE_DATA_BLOG", payload: responseAPI.data });
+      dispatch({
+        type: "UPDATE_PAGE",
+        payload: {
+          currentPage: responseAPI.current_page,
+          totalPage: Math.ceil(responseAPI.total_data / responseAPI.per_page), // Math.ceil doing pembulatan ke atas
+        },
+      });
     })
     .catch((err) => {
       console.log("error: ", err);
