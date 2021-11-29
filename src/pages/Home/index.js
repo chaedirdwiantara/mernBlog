7727b938c18f011718b956f8b7c08ed1c27521b8
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-11-29 16:48:52 +0700

    add new post in upload image on create-blog page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useHistory } from "react-router-dom";
-import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setDataBlog } from "../../config/redux/action";
 
